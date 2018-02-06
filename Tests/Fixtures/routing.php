21dcc63d29ad46cb23ec54d4c7416dd9fdc9a46d
andrey1s <andrey@4devs.pro>
2018-02-06 09:59:31 +0300

    php cs

--- Tests/Fixtures/routing.php
+++ Tests/Fixtures/routing.php
@@ -1,4 +1,12 @@
 <?php
+
+/*
+ * This file is part of the 4devs Export Routing package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 $collection = new \Symfony\Component\Routing\RouteCollection();
 
 $collection->add('homepage', new \Symfony\Component\Routing\Route('/'));
--- Tests/RoutesExtractorTest.php
+++ Tests/RoutesExtractorTest.php
@@ -1,18 +1,24 @@
 <?php
 
+/*
+ * This file is part of the 4devs Export Routing package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace FDevs\ExportRouting\Tests;
 
 use FDevs\ExportRouting\Extractor\ExposedInterface;
 use FDevs\ExportRouting\Extractor\OptionExposed;
 use FDevs\ExportRouting\RoutesExtractor;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Routing\Loader\PhpFileLoader;
 use Symfony\Component\Routing\Router;
-use Symfony\Component\Config\FileLocator;
 
 class RoutesExtractorTest extends TestCase
 {
-
     public function testEmptyGetRoutes()
     {
         $extractor = $this->createRoutesExtractor('routing.php');
@@ -32,16 +38,14 @@ class RoutesExtractorTest extends TestCase
         $extractor = $this->createRoutesExtractor('routing.php');
         $context = $extractor->getContext();
         $this->assertEquals('', $context->getBaseUrl());
-
     }
 
     private function createRoutesExtractor(string $resource = 'routing.php', ExposedInterface $exposed = null)
     {
-        $loader = new PhpFileLoader(new FileLocator(array(__DIR__ . '/Fixtures')));
+        $loader = new PhpFileLoader(new FileLocator([__DIR__.'/Fixtures']));
         $router = new Router($loader, $resource);
         $exposed = $exposed ?? new OptionExposed();
+
         return new RoutesExtractor($router, $exposed);
-
     }
-
 }
